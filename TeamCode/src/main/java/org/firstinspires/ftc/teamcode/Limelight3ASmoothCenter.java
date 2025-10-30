730e39c7830ce3a7848fa8536530cb44e9d194fc
3796experimental <charlielarson23@gmail.com>
2025-10-30 10:33:49 -0400

    whee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight3ASmoothCenter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight3ASmoothCenter.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "Limelight3A Smooth Center", group = "Concept")
+@TeleOp
 public class Limelight3ASmoothCenter extends LinearOpMode {
 
     private DcMotor turnMotor;
