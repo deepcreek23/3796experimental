9d36b6d7c0cd0727ef74f5f6b3b3cd78b1f2f876
3796experimental <charlielarson23@gmail.com>
2025-11-22 12:58:02 -0500

    major code update?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/finalDriveClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/finalDriveClass.java
@@ -8,7 +8,14 @@ public class finalDriveClass extends OpMode {
     private DcMotor intake;
     private DcMotor turn;
     private DcMotor shoot;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
     double mult;
+    double velo2;
+    double velo3;
+
 
     @Override
     public void init() {
@@ -21,12 +28,35 @@ public class finalDriveClass extends OpMode {
         shoot = hardwareMap.get(DcMotor.class, "port0");
         shoot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         shoot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight = hardwareMap.get(DcMotor.class, "eHub0");
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight = hardwareMap.get(DcMotor.class, "eHub1");
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft = hardwareMap.get(DcMotor.class, "eHub2");
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft = hardwareMap.get(DcMotor.class, "eHub3");
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
     }
 
     @Override
     public void loop() {
         double velocity = mult*(gamepad1.right_trigger - gamepad1.left_trigger);
-        double velo2 = mult*(gamepad1.left_stick_x);
+        if(gamepad1.a){
+            velo2 = mult;
+        }else if(gamepad1.b){
+            velo2 = -1 * mult;
+        }
+        if(gamepad1.x){
+            velo3 = mult;
+        } else if(gamepad1.y){
+            velo3 = -1 * mult;
+        }
         double velo3 = mult*(gamepad1.right_stick_x);
         intake.setPower(velo3);
         turn.setPower(-velo2 / 2);
@@ -39,5 +69,28 @@ public class finalDriveClass extends OpMode {
         } else if (gamepad1.dpadUpWasReleased()) {
             mult = mult +0.05;
         }
+
+        if(mult >= 1){
+            mult = 1;
+        } else if (mult <= 0){
+            mult = 0;
+        }
+        double forward =-this.gamepad1.left_stick_y;
+        double right = this.gamepad1.left_stick_x;
+        double turn = this.gamepad1.right_stick_x;
+
+        frontRight.setPower(mult *(forward - right - turn));
+        frontLeft.setPower(mult * (forward + right + turn));
+        backRight.setPower(mult * (forward + right - turn));
+        backLeft.setPower(mult * (forward - right + turn));
+
+        if(forward == 0){
+            telemetry.addData("Forward", -forward);
+        }else{
+            telemetry.addData("Forward", forward);
+        }
+        telemetry.addData("Right", right);
+        telemetry.addData("Turn", turn);
+        telemetry.addData("Speed", mult);
     }
 }
